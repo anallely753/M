23de0de2736aa93620d8ef269b790fbe82f1131b
anallely753 <anallely.0710@gmail.com>
2019-03-29 10:37:08 -0600

    menu y contact

--- js/app.js
+++ js/app.js
@@ -1,17 +1,2 @@
   M.AutoInit();
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-$(document).ready(function(){
-    $('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
-  });
-  });
